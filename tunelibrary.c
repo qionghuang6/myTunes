3958bb22934013d89535109b31b80eb8d75ee5c0
Devin Lin <dlin01@stuy.edu>
2019-11-04 00:10:37 -0500

    search song

--- tunelibrary.c
+++ tunelibrary.c
@@ -10,11 +10,30 @@ void initialize_library(library library) {
   }
 }
 
-void add_song(struct song_node * song) {
-  char artist_first_initial = (song->artist)[0];
+void add_song(library library, char* song, char* artist) {
+  char artist_first_initial = artist[0];
+  // this is for uppercase artist names
   if (artist_first_initial >= 65 && artist_first_initial <= 90) {
-    int index = artist_first_initial - 64; // this is for uppercase artist names
-    //library[index].insert_front(song, song->artist, song->name);
-    printf("%d\n",sizeof(library));
+    int index = artist_first_initial - 64;
+    library[index] = insert_ordered(library[index], song, artist);
+  } if else (artist_first_initial >= 97 && artist_first_initial <= 122) {
+    int index = artist_first_initial - 96;
+    library[index] = insert_ordered(library[index], song, artist);
+  } else {
+    library[26] = insert_ordered(library[26], song, artist);
+  }
+}
+
+struct song_node * search_song(library library, char * song, char * artist) {
+  char artist_first_initial = artist[0];
+  // this is for uppercase artist names
+  if (artist_first_initial >= 65 && artist_first_initial <= 90) {
+    int index = artist_first_initial - 64;
+
+  }
+  // this is for lowercase artist names
+  if (artist_first_initial >= 97 && artist_first_initial <= 122) {
+    int index = artist_first_initial - 96;
+
   }
 }
--- tunelibrary.h
+++ tunelibrary.h
@@ -1,12 +1,12 @@
 typedef struct song_node * library[27]; // now a library is an array of 27 song_nodes
 
 void initialize_library(library library);
-void add_song(struct song_node * song);
-struct song_node * search_song(char * song, char * artist);
-struct song_node * search_artist(char * artist);
-void print_letter(char letter);
-void print_artist(char * artist);
+void add_song(library library, char* song, char* artist);
+struct song_node * search_song(library library, char * song, char * artist);
+struct song_node * search_artist(library library, char * artist);
+void print_letter(library library, char letter);
+void print_artist(library library, char * artist);
 void print_library(library library);
 void shuffle(library library, int total_songs);
-void delete_song(char * song);
+void delete_song(library library, char * song);
 void clear_library(library library);
