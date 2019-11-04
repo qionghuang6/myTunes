b97640b8bf7a75aa36c853876f4a3397b8470cd5
Devin Lin <dlin01@stuy.edu>
2019-11-03 23:55:07 -0500

    changed and added init library

--- tunelibrary.c
+++ tunelibrary.c
@@ -4,7 +4,17 @@
 #include "linkedlist.h"
 #include "tunelibrary.h"
 
+void initialize_library(library library) {
+  for (int i = 0; i < 27; i++) {
+    library[i] = NULL;
+  }
+}
+
 void add_song(struct song_node * song) {
   char artist_first_initial = (song->artist)[0];
-  printf("%c\n", artist_first_initial);
+  if (artist_first_initial >= 65 && artist_first_initial <= 90) {
+    int index = artist_first_initial - 64; // this is for uppercase artist names
+    //library[index].insert_front(song, song->artist, song->name);
+    printf("%d\n",sizeof(library));
+  }
 }
--- tunelibrary.h
+++ tunelibrary.h
@@ -1,11 +1,12 @@
-struct song_node * library[27];
+typedef struct song_node * library[27]; // now a library is an array of 27 song_nodes
 
+void initialize_library(library library);
 void add_song(struct song_node * song);
 struct song_node * search_song(char * song, char * artist);
 struct song_node * search_artist(char * artist);
 void print_letter(char letter);
 void print_artist(char * artist);
-void print_library();
-void shuffle();
+void print_library(library library);
+void shuffle(library library, int total_songs);
 void delete_song(char * song);
-void clear_library();
+void clear_library(library library);
