f4028a69250d1a65c0b9a4652d26f84c11d1ee6e
qionghuang6 <qhuang00@stuy.edu>
2019-11-04 11:31:31 -0500

    idk whats wrong with add song here

--- main.c
+++ main.c
@@ -6,55 +6,57 @@
 
 int main() {
   srand(time(NULL));
-  struct song_node * list;
-  //
-  // printf("LINKED LIST TESTS\n==============\n");
-  // list = insert_front(list, "Kanye West", "Follow God");
-  // list = insert_front(list,"Post Malone", "Circles");
-  // list = insert_front(list,"Travis Scott", "HIGHEST IN THE ROOM");
-  // list = insert_front(list,"Selena Gomez", "Lose You To Love Me");
-  // list = insert_front(list,"Maroon 5", "Memories");
-  // printf("\nTesting Print List:\n" );
-  // print_list(list);
-  //
-  // printf("==============\n\nTesting Print NOde\n");
-  // print_node(list);
-  //
-  // printf("==============\n\nTesting Find Node\n");
-  // printf("Looking for Kanye West: Follow God\n");
-  // find_node(list,"Kanye West", "Follow God");
-  // printf("Looking for Travis Scott: HIGHEST IN THE ROOM\n");
-  // find_node(list,"Travis Scott", "HIGHEST IN THE ROOM");
-  //
-  // printf("==============\n\nTesting Find Artist\n");
-  // printf("Looking for Kanye West\n");
-  // print_list(find_author(list, "Kanye West"));
-  // printf("Looking for Selena Gomez\n");
-  // print_list(find_author(list, "Selena Gomez"));
-  // printf("Looking for John-Alf Dyrland Weaver\n");
-  // print_list(find_author(list, "John-Alf Dyrland Weaver"));
-  //
-  // printf("==============\n\nWe didn't use a songcmp\n");
-  // printf("===============\n\n Testing random\n");
-  // print_node(rand_node(list));
-  // print_node(rand_node(list));
-  // print_node(rand_node(list));
-  // print_node(rand_node(list));
-  //
-  // printf("===============\n\n Testing remove\n");
-  // printf("Removing Kanye West: Follow God\n" );
-  // list = myremove(list, "Kanye West", "Follow God");
-  // print_list(list);
-  // printf("Removing Maroon 5: Memories\n");
-  // list = myremove(list,"Maroon 5", "Memories");
-  // print_list(list);
-  // printf("Removing Post Malone: Circles\n" );
-  // list = myremove(list,"Post Malone", "Circles");
-  //
-  // printf("===============\n\n Testing free list\n");
-  // list = free_list(list);
-  // print_list(list);
-  // printf("===============\n");
+  struct song_node * list = NULL;
+
+  print_list(list);
+  printf("LINKED LIST TESTS\n==============\n");
+  list = insert_ordered(list, "Kanye West", "Follow God");
+  list = insert_ordered(list,"Post Malone", "Circles");
+  list = insert_ordered(list,"Travis Scott", "HIGHEST IN THE ROOM");
+  list = insert_ordered(list,"Selena Gomez", "Lose You To Love Me");
+  list = insert_ordered(list,"Maroon 5", "Memories");
+  printf("\nTesting Print List:\n" );
+  print_list(list);
+
+  printf("==============\n\nTesting Print NOde\n");
+  print_node(list);
+
+  printf("==============\n\nTesting Find Node\n");
+  printf("Looking for Kanye West: Follow God\n");
+  find_node(list,"Kanye West", "Follow God");
+  printf("Looking for Travis Scott: HIGHEST IN THE ROOM\n");
+  find_node(list,"Travis Scott", "HIGHEST IN THE ROOM");
+
+  printf("==============\n\nTesting Find Artist\n");
+  printf("Looking for Kanye West\n");
+  print_list(find_author(list, "Kanye West"));
+  printf("Looking for Selena Gomez\n");
+  print_list(find_author(list, "Selena Gomez"));
+  printf("Looking for John-Alf Dyrland Weaver\n");
+  print_list(find_author(list, "John-Alf Dyrland Weaver"));
+
+  printf("==============\n\nWe didn't use a songcmp\n");
+  printf("===============\n\n Testing random\n");
+  print_node(rand_node(list));
+  print_node(rand_node(list));
+  print_node(rand_node(list));
+  print_node(rand_node(list));
+
+  printf("===============\n\n Testing remove\n");
+  printf("Removing Kanye West: Follow God\n" );
+  list = myremove(list, "Kanye West", "Follow God");
+  print_list(list);
+  printf("Removing Maroon 5: Memories\n");
+  list = myremove(list,"Maroon 5", "Memories");
+  print_list(list);
+  printf("Removing Post Malone: Circles\n" );
+  list = myremove(list,"Post Malone", "Circles");
+
+  print_list(list);
+  printf("===============\n\n Testing free list\n");
+  //list = free_list(list);
+  print_list(list);
+  printf("===============\n");
 
   printf("-------LIBRARY TEST-------\n");
 
@@ -72,30 +74,30 @@ int main() {
   add_song(mytunes, "u-song", "3-artist");
 
   printf("Testing Print library\n");
-  print_library(mytunes);
-
+  //print_library(mytunes);
+  //
   printf("Testing Print letter\n");
   print_letter(mytunes, 'b');
-
-  printf("Testing find\n");
-  printf("looking for [zoo-artist : t-song]\n");
-  search_song(mytunes, "t-song", "zoo-artist");
-  printf("looking for [zoo : t]\n");
-  search_song(mytunes, "t", "zoo");
-
-  printf("Testing remove\n");
-  printf("removing : [zoo-artist : t-song]\n");
-  delete_song(mytunes, "t-song", "zoo-artist");
-  print_library(mytunes);
-
-  printf("Testing Print artist\n");
-  print_artist(mytunes, "b-artist");
-
-  printf("Testing shuffle\n");
-  shuffle(mytunes, 5);
-
-  printf("Testing clear_library\n");
-  clear_library(mytunes);
-  print_library(mytunes);
+  //
+  // printf("Testing find\n");
+  // printf("looking for [zoo-artist : t-song]\n");
+  // search_song(mytunes, "t-song", "zoo-artist");
+  // printf("looking for [zoo : t]\n");
+  // search_song(mytunes, "t", "zoo");
+  //
+  // printf("Testing remove\n");
+  // printf("removing : [zoo-artist : t-song]\n");
+  // delete_song(mytunes, "t-song", "zoo-artist");
+  // print_library(mytunes);
+  //
+  // printf("Testing Print artist\n");
+  // print_artist(mytunes, "b-artist");
+  //
+  // printf("Testing shuffle\n");
+  // shuffle(mytunes, 5);
+  //
+  // printf("Testing clear_library\n");
+  // clear_library(mytunes);
+  // print_library(mytunes);
   return 0;
 }
